96d7a4c7c34ed309c080054389dd7eee149daa8d
MattRickS <mattalexshaw@gmail.com>
2020-03-31 21:32:24 +0100

    implement heuristic method

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -1,4 +1,5 @@
 #include "route_planner.h"
+#include "route_model.h"
 #include <algorithm>
 
 RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y) : m_Model(model)
@@ -14,13 +15,9 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
     end_node = &m_Model.FindClosestNode(end_x, end_y);
 }
 
-// TODO 3: Implement the CalculateHValue method.
-// Tips:
-// - You can use the distance to the end_node for the h value.
-// - Node objects have a distance method to determine the distance to another node.
-
 float RoutePlanner::CalculateHValue(RouteModel::Node const *node)
 {
+    return node->distance(*end_node);
 }
 
 // TODO 4: Complete the AddNeighbors method to expand the current node by adding all unvisited neighbors to the open list.
